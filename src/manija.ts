c0da97ea846f680390e86b715b950166b7e1734e
Dante Orti <uarjoldanteorti@gmail.com>
2026-06-03 14:31:23 -0300

    manijan't

--- src/manija.ts
+++ src/manija.ts
@@ -1,10 +1,14 @@
 import { preguntar } from './lib/consola.ts';
 
+let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+Number(tarjetaTexto);
+
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
-  return 0; // COMPLETAR
+
+  return numero/10^(16-tamañoPrefijo);
 }
 
-let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 let red: string = ''; // COMPLETAR
 console.log(red);
