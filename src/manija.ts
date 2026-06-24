1a983b4db0a65fa3a63ad92da31e4535ca424220
Dante Orti <uarjoldanteorti@gmail.com>
2026-06-24 08:54:21 -0300

    Manija hecho funcional

--- src/manija.ts
+++ src/manija.ts
@@ -1,14 +1,35 @@
 import { preguntar } from './lib/consola.ts';
 
-let tarjetaTexto: string = preguntar('Ingresá el número de tarjeta: ');
+let tarjetaTexto: string = preguntar('Ingresar el numero de tarjeta: ');
 Number(tarjetaTexto);
+let tamañoPrefijo: string = preguntar('Cuantos digitos tiene el prefijo? ');
 
 function obtenerPrefijo(numero: number, tamañoPrefijo: number): number {
-
-  return numero/10^(16-tamañoPrefijo);
+  return Math.floor(numero / 10 ** (16-tamañoPrefijo));
 }
 
-//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+let prefijo: number = obtenerPrefijo(Number(tarjetaTexto), Number(tamañoPrefijo));
 
-let red: string = ''; // COMPLETAR
+let red: string = 'No funciona';
+
+if(Number(tamañoPrefijo) === 2){
+  if(prefijo === 34 || prefijo === 37){
+  red = 'Amex';
+  }
+  else if(prefijo === 51 || prefijo === 52 || prefijo === 53 || prefijo === 54 || prefijo === 55){
+    red = 'Mastercard';
+  }
+  else{
+    red = 'Error';
+  }
+}
+else if(Number(tamañoPrefijo) === 1){
+  if(prefijo === 4){
+    red = 'Visa';
+  }
+  else{
+    red = 'Error';
+  }
+}
+
 console.log(red);
